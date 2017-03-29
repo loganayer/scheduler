c6a6533f84e4d485268ff9dced0d31b86a8cabd2
Logan Ayer <xodiee@yahoo.com>
2017-03-29 18:16:33 -0500

    hopefully done

--- scheduler/src/libpriqueue/libpriqueue.c
+++ scheduler/src/libpriqueue/libpriqueue.c
@@ -9,7 +9,7 @@
 
 /**
   Initializes the priqueue_t data structure.
-  
+
   Assumtions
     - You may assume this function will only be called once per instance of priqueue_t
     - You may assume this function will be the first function called using an instance of priqueue_t.
@@ -21,10 +21,10 @@ void priqueue_init(priqueue_t *q, int(*comparer)(const void *, const void *))
 {
 	/*initialize size to 0*/
 	q->size = 0;
-	
+
 	/*allocate array to hold our heap*/
 	q->heap = malloc(sizeof(void *) * 100);
-	
+
 	/*initialize using comparator passed in*/
 	q->comparator = comparer;
 }
@@ -46,7 +46,7 @@ int priqueue_offer(priqueue_t *q, void *ptr)
 
 	/*qsort the array*/
 	qsort(q->heap, q->size, sizeof(void*), q->comparator);
-	
+
 	for (int i = 0; i < q->size; i++)
 	{
 		if (q->heap[i] == ptr)
@@ -54,7 +54,7 @@ int priqueue_offer(priqueue_t *q, void *ptr)
 			return i;
 		}
 	}
-	
+
 	/*shouldn't happen*/
 	return -1;
 }
@@ -63,7 +63,7 @@ int priqueue_offer(priqueue_t *q, void *ptr)
 /**
   Retrieves, but does not remove, the head of this queue, returning NULL if
   this queue is empty.
- 
+
   @param q a pointer to an instance of the priqueue_t data structure
   @return pointer to element at the head of the queue
   @return NULL if the queue is empty
@@ -84,7 +84,7 @@ void *priqueue_peek(priqueue_t *q)
 /**
   Retrieves and removes the head of this queue, or NULL if this queue
   is empty.
- 
+
   @param q a pointer to an instance of the priqueue_t data structure
   @return the head of this queue
   @return NULL if this queue is empty
@@ -105,7 +105,7 @@ void *priqueue_poll(priqueue_t *q)
 /**
   Returns the element at the specified position in this list, or NULL if
   the queue does not contain an index'th element.
- 
+
   @param q a pointer to an instance of the priqueue_t data structure
   @param index position of retrieved element
   @return the index'th element in the queue
@@ -125,10 +125,10 @@ void *priqueue_at(priqueue_t *q, int index)
 
 
 /**
-  Removes all instances of ptr from the queue. 
-  
+  Removes all instances of ptr from the queue.
+
   This function should not use the comparer function, but check if the data contained in each element of the queue is equal (==) to ptr.
- 
+
   @param q a pointer to an instance of the priqueue_t data structure
   @param ptr address of element to be removed
   @return the number of entries removed
@@ -136,30 +136,23 @@ void *priqueue_at(priqueue_t *q, int index)
 int priqueue_remove(priqueue_t *q, void *ptr)
 {
 	int count = 0;
-	
+
 	/*probe all elements of array*/
 	for (int i = 0; i < q->size; i++)
 	{
 		/*we found a value so we must delete*/
 		if (ptr == q->heap[i])
 		{
-			/*replace with last value in array*/
-			q->heap[i] = q->heap[q->size-1];
-			
-			/*decrement size*/
-			q->size--;
-			
+			priqueue_remove_at(q, i);
+
 			/*decrement i so we check the again value as there
 			is a small chance this value was replaced with value*/
 			i--;
-			
+
 			count++;
 		}
 	}
-	
-	/*qsort again to make sure order is upheld*/
-	qsort(q->heap, q->size, sizeof(void*), q->comparator);
-	
+
 	return count;
 }
 
@@ -167,7 +160,7 @@ int priqueue_remove(priqueue_t *q, void *ptr)
 /**
   Removes the specified index from the queue, moving later elements up
   a spot in the queue to fill the gap.
- 
+
   @param q a pointer to an instance of the priqueue_t data structure
   @param index position of element to be removed
   @return the element removed from the queue
@@ -177,16 +170,16 @@ void *priqueue_remove_at(priqueue_t *q, int index)
 {
 	/*temp holds value at index*/
 	void* temp = q->heap[index];
-	
-	/* replace this index with last index*/
-    q->heap[index] = q->heap[q->size-1];
- 
+
+	/* replace this index and shift elements*/
+    for (int i = index; i < q->size; i++)
+    {
+      q->heap[i] = q->heap[i+1];
+    }
+
     /*Decrease heap size by 1*/
     q->size--;
- 
-    /*qsort to restore order*/
-	qsort(q->heap, q->size, sizeof(void*), q->comparator);
-	
+
 	/*return value at index*/
 	return temp;
 }
@@ -194,7 +187,7 @@ void *priqueue_remove_at(priqueue_t *q, int index)
 
 /**
   Returns the number of elements in the queue.
- 
+
   @param q a pointer to an instance of the priqueue_t data structure
   @return the number of elements in the queue
  */
@@ -206,7 +199,7 @@ int priqueue_size(priqueue_t *q)
 
 /**
   Destroys and frees all the memory associated with q->
-  
+
   @param q a pointer to an instance of the priqueue_t data structure
  */
 void priqueue_destroy(priqueue_t *q)
